92a65112987d3d3557292da959b17c76fc8a54a1
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2015-02-26 11:36:03 +1000

    Working code

--- src/cSharp/Program.cs
+++ src/cSharp/Program.cs
@@ -22,13 +22,15 @@ namespace ConsoleApplication1
 
             String authorization = String.Format("{0}:{1}", KEY, PASSWORD);
             String base64credentials = Convert.ToBase64String(new ASCIIEncoding().GetBytes(authorization));
-
-            BitPOS.Models.Order order = new BitPOS.Models.Order() { amount = 200, currency = "AUD", reference = "BitcoinBrisbane" };
+            
+            //Note fields mandatory otherwise 500 error
+            BitPOS.Models.Order order = new BitPOS.Models.Order() { amount = 200, currency = "AUD", reference = "BitcoinBrisbane", description="Test Ticket", failureURL="https://www.bitcoinbrisbane.com.au/fail/1", successURL="https://www.bitcoinbrisbane.com.au/greatsuccess/1" };
             String json = JsonConvert.SerializeObject(order);
 
             WebClient webClient = new WebClient() { Credentials = new NetworkCredential(KEY, PASSWORD) };
             webClient.Headers[HttpRequestHeader.Authorization] = string.Format("Basic {0}", base64credentials);
-
+            webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+            
             if (isTestNet == true)
             {
                 //Use this for test because of SSL issues
